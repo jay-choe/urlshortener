9d8b1e81e67230c36cadb6f59b3710d328bb67e6
최재형 <dang0764@naver.com>
2021-07-28 01:11:55 +0900

    initial commit

---
+++ src/main/java/com/visitor/urlshortener/UrlshortenerApplication.java
@@ -0,0 +1,13 @@
+package com.visitor.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshortenerApplication.class, args);
+	}
+
+}
