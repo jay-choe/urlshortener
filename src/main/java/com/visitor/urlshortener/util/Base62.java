89cbaad2677c54b2e0ace046087f9e0377bbac57
최재형 <dang0764@naver.com>
2021-08-04 01:41:46 +0900

    VSTR-38 Fix variable name
    - uniqueKey -> hashValue

--- src/main/java/com/visitor/urlshortener/util/Base62.java
+++ src/main/java/com/visitor/urlshortener/util/Base62.java
@@ -8,17 +8,17 @@ public class Base62 {
     private final int BASE = 62;
     public  final String BASE_FORMAT = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    public String encoding(BigInteger uniqueKey) {
+    public String encoding(BigInteger hashValue) {
 
-        if (uniqueKey.compareTo(BigInteger.ZERO) == 0) {
+        if (hashValue.compareTo(BigInteger.ZERO) == 0) {
             return String.valueOf(BASE_FORMAT.charAt(0));
         }
 
         StringBuffer sb = new StringBuffer();
 
-        while (uniqueKey.compareTo(BigInteger.ZERO) > 0) {
-            sb.append(BASE_FORMAT.charAt((uniqueKey.mod(BigInteger.valueOf(BASE)).intValue())));
-            uniqueKey = uniqueKey.divide(BigInteger.valueOf(BASE));
+        while (hashValue.compareTo(BigInteger.ZERO) > 0) {
+            sb.append(BASE_FORMAT.charAt((hashValue.mod(BigInteger.valueOf(BASE)).intValue())));
+            hashValue = hashValue.divide(BigInteger.valueOf(BASE));
         }
         return sb.reverse().toString();
     }
