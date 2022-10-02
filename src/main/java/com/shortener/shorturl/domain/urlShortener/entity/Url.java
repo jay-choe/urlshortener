ca3d8bedbbf6c3ca80d5f075e379ae13c73a6e26
jay-choe <jay0764@naver.com>
2022-10-02 22:40:11 +0900

    add create custom url test

--- src/main/java/com/shortener/shorturl/domain/urlShortener/entity/Url.java
+++ src/main/java/com/shortener/shorturl/domain/urlShortener/entity/Url.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@EqualsAndHashCode
 public class Url {
     @Id
     private String target;
---
+++ src/test/java/com/shortener/shorturl/application/shortUrl/service/ShortenerServiceTest.java
@@ -0,0 +1,53 @@
+package com.shortener.shorturl.application.shortUrl.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+import com.shortener.shorturl.application.shortUrl.exception.AlreadyExistException;
+import com.shortener.shorturl.domain.urlShortener.entity.Url;
+import com.shortener.shorturl.domain.urlShortener.repository.UrlRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class ShortenerServiceTest {
+
+    @Mock
+    UrlRepository repository;
+
+    @InjectMocks
+    ShortenerService service;
+
+    @BeforeAll
+    void init() {
+        MockitoAnnotations.openMocks(this);
+
+        Url url = Url.builder()
+            .originalUrl("http://localhost/")
+            .target("test")
+            .build();
+
+        given(repository.findById("test"))
+            .willReturn(Optional.ofNullable(url));
+
+        given(repository.save(url))
+            .willReturn(url);
+    }
+
+    @Test
+    void checkAlreadyExistTarget() {
+        assertThrows(AlreadyExistException.class, () -> service.checkAlreadyExistTarget("test"));
+    }
+
+    @Test
+    void createCustomUrl() {
+        String customUrl = service.createCustomUrl("http://localhost/", "test");
+        assertEquals("test", customUrl);
+    }
+}
