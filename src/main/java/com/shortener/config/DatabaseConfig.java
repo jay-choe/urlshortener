12e94717df8dcdfaeb6a6ef5a364be99b4c5eeef
jay-choe <jay0764@naver.com>
2022-11-27 09:40:59 +0900

    add database config

---
+++ src/main/java/com/shortener/config/DatabaseConfig.java
@@ -0,0 +1,16 @@
+package com.shortener.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EntityScan(basePackages = "com.shortener.shorturl.domain")
+@EnableJpaRepositories(basePackages = "com.shortener.shorturl.domain")
+@EnableJpaAuditing
+@EnableTransactionManagement
+public class DatabaseConfig {
+
+}
